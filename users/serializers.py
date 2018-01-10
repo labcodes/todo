7602d1025990bc8d0e75862795c2200f70c7341c
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-10 16:16:56 -0300

    Create user serializer

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ users/tests/test_serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import authenticate
+from django.test import TestCase
+from model_mommy import mommy
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserSerializerTest(TestCase):
+
+    def test_serializer_returns_correct_data(self):
+        user = mommy.make(User)
+        serializer = UserSerializer(instance=user)
+        self.assertEqual(serializer.data, {'email': user.email})
+
+    def test_serializer_creates_user_correctly(self):
+        data = {'email': 'mail@mail.com', 'password': '1q2w3e4r5t'}
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertEqual(user.email, data['email'])
+        self.assertNotEqual(user.password, data['password'])
+        self.assertIsInstance(
+            authenticate(username=data['email'], password=data['password']),
+            User
+        )
