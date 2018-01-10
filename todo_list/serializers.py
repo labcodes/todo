3c215b6759699ca6dec3ee46f703495a54494f9c
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-10 18:25:20 -0300

    Create To-Do list serializer

---
+++ todo_list/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import TodoList
+
+
+class TodoListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TodoList
+        fields = ['name']
