119c9f5d28f1f4373185899381f5a3a3c182ba5a
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-11 17:07:17 -0300

    Create Task model

--- todo_list/models.py
+++ todo_list/models.py
@@ -11,3 +11,25 @@ class TodoList(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(_('Name'), max_length=255)
+    todo_list = models.ForeignKey(
+        'todo_list.TodoList',
+         verbose_name=_('To-Do List'),
+         on_delete=models.CASCADE
+    )
+    due_date = models.DateField(_('Due Date'), null=True, blank=True)
+    owner = models.ForeignKey(
+        'users.User',
+         verbose_name=_('Owner'),
+         on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'To-Do List'
+        verbose_name_plural = 'To-Do Lists'
+
+    def __str__(self):
+        return self.name
