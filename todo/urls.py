fc30fbfc393b560f16e01092c4f410b338ff8156
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-12 14:19:38 -0300

    Add api version to token url

--- todo/urls.py
+++ todo/urls.py
@@ -20,6 +20,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_jwt_token),
+    re_path('api/(?P<version>(v1.0))/api-token-auth/', obtain_jwt_token),
     re_path('api/(?P<version>(v1.0))/users/', include('users.urls', namespace='users')),
 ]
