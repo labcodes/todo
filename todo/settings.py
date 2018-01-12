d267a8b787a5fe01f7d18fcc592ffbafae08230a
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-12 16:40:52 -0300

    Create django app for rendering react app template

--- todo/settings.py
+++ todo/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.child('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
