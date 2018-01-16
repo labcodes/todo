f6928121d5cd6e3ad28031fee2859c7da10b8913
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-16 18:12:49 -0300

    Refactor webpack config to build css properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,47 @@
-var path = require("path")
-var webpack = require('webpack')
-var BundleTracker = require('webpack-bundle-tracker')
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+var ExtractText = require('extract-text-webpack-plugin');
+var UgliFyJS = require('uglifyjs-webpack-plugin');
+
 
 module.exports = {
-context: __dirname,
+  entry: './index',
 
-entry: './assets/js/index.js',
+  output: {
+    path: path.join(__dirname, 'assets/dist'),
+    filename: 'main.js'
+  },
 
-output: {
-    path: path.resolve('./assets/bundles/'),
-    filename: "[name]-[hash].js",
-},
-
-plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}),
-],
-
-module: {
-    loaders: [
-    { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-    { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-
+  plugins: [
+    new BundleTracker({
+      path: __dirname,
+      filename: 'webpack-stats.json'
+    }),
+    new ExtractText({
+      filename: 'main.css'
+    }),
+    new UgliFyJS()
+  ],
+  
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractText.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
     ],
-},
-
+  }
 }
