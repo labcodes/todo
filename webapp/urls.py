71e58c170a17c912c762f727ce6ce8f2cd802568
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-18 10:59:17 -0300

    Simplify frontend template rendering

--- webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-from django.urls import path
-
-
-app_name = 'webapp'
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='webapp/index.html')),
-]
