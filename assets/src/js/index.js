a7bcada089fb22054a9d80808a2c5550f5e0f650
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-26 18:08:18 -0300

    Add provider with redux store to root component

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,10 +1,17 @@
+
 import React, { Component } from 'react';
-
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 
-import App from './containers/App'
-
+import App from './containers/App';
+import store from './store';
 
 import '../scss/index.scss'
 
-ReactDOM.render(<App />, document.getElementById('react-app'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+  document.getElementById('react-app')
+);
--- assets/src/js/store/index.js
+++ assets/src/js/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import todoApp from '../reducers';
 
-let store = createStore(todoApp);
+let store = createStore(todoApp);  // Create store from reducers
 
 export default store;
