636e67175a08c8a8115c1d439c6497b75031f8bd
Mariana Bedran Lesche <maribedran@gmail.com>
2018-01-30 14:16:39 -0300

    Add store for using redux devtools

--- assets/src/js/store/index.js
+++ assets/src/js/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import todoApp from '../reducers';
 
-let store = createStore(todoApp);  // Create store from reducers
+let store = createStore(
+    todoApp,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);  // Create store from reducers
 
 export default store;
