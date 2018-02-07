3124e30c224f65e0ffb83456b6652ed0e6f196ee
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-07 15:21:13 -0300

    Handle errors in todo form showing alert

--- assets/src/js/components/TodoForm/index.js
+++ assets/src/js/components/TodoForm/index.js
@@ -13,13 +13,14 @@ import {
 import APIService from '../../services/APIService';
 import store from '../../store';
 import { addTodo } from '../../actions/todo';
+import { setAlert, removeAlert } from '../../actions/alert-message';
 
 
 class TodoForm extends React.Component {
     constructor (props) {
         super(props);
-        this.state = {id: props.id, name: props.name || ''};
 
+        this.state = {id: props.id, name: props.name || ''};
         this.setName = this.setName.bind(this);
     }
 
@@ -36,15 +37,47 @@ class TodoForm extends React.Component {
     create () {
         const api = new APIService();
         api.createTodo(this.state)
-        .then(todo => store.dispatch(addTodo(todo)))
-        .catch(response => {debugger;})
+        .then(todo => {
+            store.dispatch(addTodo(todo));
+            this.handleSuccess();
+        })
+        .catch(response => this.handleError(response))
     }
 
     update () {
         const api = new APIService();
         api.updateTodo(this.state)
-        .then(response => {debugger;})
-        .catch(response => {debugger;})
+        .then(response => this.handleSuccess())
+        .catch(response => this.handleError(response))
+    }
+
+    handleSuccess () {
+        const success = {
+            type: 'SUCCESS',
+            message: 'ToDo has been saved successfully!'
+        };
+        store.dispatch(setAlert(success));
+    }
+
+    handleError (error) {
+        store.dispatch(setAlert({
+            type: 'ERROR',
+            message: this.formatError(error)
+        }));
+        setTimeout(
+            () => {store.dispatch(removeAlert())},
+            5000
+        );
+    }
+
+    formatError (error) {
+        let messages = [];
+        Object.keys(error).forEach((key, index) => {
+            let name = key.charAt(0).toUpperCase() + key.slice(1);
+            let message = error[key].join(' ');
+            messages.push(`${name}: ${message}`)
+        });
+        return messages.join('<br />');
     }
 
     setName (event) {
