75a4ddbf3df51bc2745468b0e3221ea8ea365212
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-15 17:39:31 -0300

    Refactor todo form buttons styles

--- assets/src/js/components/TodoForm/index.js
+++ assets/src/js/components/TodoForm/index.js
@@ -9,10 +9,13 @@ import {
     Form,
     Input
 } from 'reactstrap';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faTrash from '@fortawesome/fontawesome-free-solid/faTrash';
+import faSave from '@fortawesome/fontawesome-free-solid/faSave';
 
 import APIService from '../../services/APIService';
 import store from '../../store';
-import { addTodo } from '../../actions/todo';
+import { addTodo, removeTodo } from '../../actions/todo';
 import { setAlert } from '../../actions/alert-message';
 
 
@@ -21,9 +24,10 @@ class TodoForm extends React.Component {
         super(props);
 
         this.state = {id: props.id, name: props.name || '', hasChanged: false};
-        this.initialProps = Object.assign({}, ...props);
+        this.initialProps = {id: props.id, name: props.name || ''};
         this.isCreation = !props.id;
         this.setName = this.setName.bind(this);
+        this.delete = this.delete.bind(this);
     }
 
     handleSubmit (e) {
@@ -41,7 +45,7 @@ class TodoForm extends React.Component {
         api.createTodo(this.state)
         .then(todo => {
             store.dispatch(addTodo(todo));
-            this.handleSuccess();
+            this.handleSuccess('created');
         })
         .catch(response => this.handleError(response))
     }
@@ -49,14 +53,24 @@ class TodoForm extends React.Component {
     update () {
         const api = new APIService();
         api.updateTodo(this.state)
-        .then(response => this.handleSuccess())
+        .then(response => this.handleSuccess('updated'))
         .catch(response => this.handleError(response))
     }
 
-    handleSuccess () {
+    delete() {
+        const api = new APIService();
+        api.deleteTodo(this.props.id)
+        .then(response => {
+            store.dispatch(removeTodo(this.props));
+            this.handleSuccess('deleted')
+        })
+        .catch(response => this.handleError(response));
+    }
+
+    handleSuccess (action) {
         const success = {
             type: 'SUCCESS',
-            message: 'ToDo has been saved successfully!'
+            message: `ToDo has been ${action} successfully!`
         };
         store.dispatch(setAlert(success));
         this.initialProps.name = this.state.name;
@@ -90,19 +104,31 @@ class TodoForm extends React.Component {
 
     render() {
         const bgClass = this.isCreation ? 'bg-light' : '';
+        const saveButton = this.state.hasChanged && (
+            <Button type="submit" outline color="primary" className="ml-1">
+                <FontAwesomeIcon icon={faSave} />
+            </Button>
+        );
+        const removeButton = !this.isCreation && (
+            <Button onClick={this.delete} outline color="danger" className="ml-1">
+                <FontAwesomeIcon icon={faTrash} />
+            </Button>
+        );
+
         return (
             <Form onSubmit={this.handleSubmit.bind(this)}>
                 <Card className={bgClass}>
                     <CardBody>
-                        <CardTitle>
+                        <div className="form-inline">
                             <Input
                                 type="text"
                                 value={this.state.name}
                                 onChange={this.setName}
                                 />
-                        </CardTitle>
+                            {saveButton}
+                            {removeButton}
+                        </div>
                         <CardText></CardText>
-                        {this.state.hasChanged && <Button type="submit" color="primary">Save</Button>}
                     </CardBody>
                 </Card>
             </Form>
