8b9f865dc74e7616984a6849714e429fdca8a8c8
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-19 11:55:11 -0300

    Add verify token view to users urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 from .views import SignUpView
 
@@ -8,5 +8,6 @@ from .views import SignUpView
 app_name='users'
 urlpatterns = [
     path('api-token-auth/', obtain_jwt_token),
+    path('api-token-verify/', verify_jwt_token),
     path('signup/', SignUpView.as_view(), name='signup')
 ]
