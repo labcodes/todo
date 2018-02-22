702a046b87f5ddbb046be14ff9b644413a40c720
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-22 18:16:07 -0300

    Correct lint on alert reducer

--- assets/src/js/reducers/alert-message/index.js
+++ assets/src/js/reducers/alert-message/index.js
@@ -1,25 +1,27 @@
 import {
-    SET_ALERT,
-    REMOVE_ALERT,
+  SET_ALERT,
+  REMOVE_ALERT,
 } from '../../actions/alert-message';
 
 
 const initialState = {
-    type: '',
-    message: ''
+  type: '',
+  message: '',
 };
 
-export const alert = (state = initialState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-        case SET_ALERT:
-            return Object.assign({}, state, {
-                type: payload.type,
-                message: payload.message
-            });
-        case REMOVE_ALERT:
-            return Object.assign({}, state, initialState);
-        default:
-            return state
-    }
-}
+const alert = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ALERT:
+      return Object.assign({}, state, {
+        type: payload.type,
+        message: payload.message,
+      });
+    case REMOVE_ALERT:
+      return Object.assign({}, state, initialState);
+    default:
+      return state;
+  }
+};
+
+export default alert;
