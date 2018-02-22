ef83311b5681ccbe3edaf27ffc75dbca89e83912
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-22 14:23:05 -0300

    Simplify object assignment on login reducer

--- assets/src/js/reducers/user/index.js
+++ assets/src/js/reducers/user/index.js
@@ -14,11 +14,7 @@ export const user = (state = initialState, action) => {
     const { type, payload } = action;
     switch (type) {
         case LOGIN_USER:
-            return Object.assign({}, state, {
-                email: payload.email,
-                name: payload.name,
-                token: payload.token
-            })
+            return Object.assign({}, state, payload)
         case LOGOUT_USER:
             return Object.assign({}, state, initialState)
         default:
