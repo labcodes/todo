31716ca84b98421eef71fad31207ad3b05c229df
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-22 17:38:08 -0300

    Correct lints on store index

--- assets/src/js/store/index.js
+++ assets/src/js/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'redux';
 import todoApp from '../reducers';
 
-let store = createStore(
-    todoApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);  // Create store from reducers
+const store = createStore(
+  todoApp,
+  /* eslint-disable no-underscore-dangle */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  /* eslint-disable no-underscore-dangle */
+);
 
 export default store;
