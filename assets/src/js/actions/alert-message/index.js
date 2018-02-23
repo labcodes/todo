b08cfb25b517d78cc9b7181e87317cec7f448099
Mariana Bedran Lesche <maribedran@gmail.com>
2018-02-23 11:38:00 -0300

    Fix lint on alert actions

--- assets/src/js/actions/alert-message/index.js
+++ assets/src/js/actions/alert-message/index.js
@@ -2,14 +2,14 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 
 export function setAlert(obj) {
-    return {
-        type: SET_ALERT,
-        payload: obj
-    }
+  return {
+    type: SET_ALERT,
+    payload: obj,
+  };
 }
 
 export function removeAlert() {
-    return {
-        type: REMOVE_ALERT
-    }
+  return {
+    type: REMOVE_ALERT,
+  };
 }
